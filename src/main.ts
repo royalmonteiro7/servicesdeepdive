3765d9ce7037aeacba121c02c858cf07bbabcf18
Royal Monteiro <royalmonteiro7@gmail.com>
2024-11-13 14:31:19 +0530

    12

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import { InjectionToken } from '@angular/core';
+import { TasksService } from './app/tasks/tasks.service';
+
+export const TasksServiceToken = new InjectionToken<TasksService>(
+  'tasks-service-token'
+);
+
+// bootstrapApplication(AppComponent, { providers: [TasksService] }).catch((err) =>
+//   console.error(err)
+// );
+
+bootstrapApplication(AppComponent, {
+  providers: [{ provide: TasksServiceToken, useClass: TasksService }],
+}).catch((err) => console.error(err));
